1fe54f46c9651b78a2b56cb144f6fff26eb0b3ab
Coinworks.info <adamandshandra@server.local>
2014-07-20 15:38:54 -0600

    Seednodes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -54,11 +54,11 @@ public:
         assert(hashGenesisBlock == uint256("0x000009f0fcbad3aac904d3660cfdcf238bf298cfe73adf1d39d14fc5c740ccc7"));
         assert(genesis.hashMerkleRoot == uint256("0xc843eae4658e3a51d2f280c36376ce56dc71a6c70e4b1c5ad2d7a9316f9b9ab7"));
 
-        vSeeds.push_back(CDNSSeedData("seed1.coinworks.info", "seed1.coinworks.info"));
-        vSeeds.push_back(CDNSSeedData("seed3.coinworks.info", "seed3.coinworks.info"));
-        vSeeds.push_back(CDNSSeedData("seed5.coinworks.info", "seed5.coinworks.info"));
-        vSeeds.push_back(CDNSSeedData("seed6.coinworks.info", "seed6.coinworks.info"));
-        vSeeds.push_back(CDNSSeedData("seed7.coinworks.info", "seed7.coinworks.info"));
+        vSeeds.push_back(CDNSSeedData("seed1.parallelcoin.info", "seed1.parallelcoin.info"));
+        vSeeds.push_back(CDNSSeedData("seed3.parallelcoin.info", "seed3.parallelcoin.info"));
+        vSeeds.push_back(CDNSSeedData("seed2.parallelcoin.info", "seed2.parallelcoin.info"));
+        vSeeds.push_back(CDNSSeedData("seed4.parallelcoin.info", "seed4.parallelcoin.info"));
+        vSeeds.push_back(CDNSSeedData("seed5.parallelcoin.info", "seed5.parallelcoin.info"));
 
         base58Prefixes[PUBKEY_ADDRESS] = 83;
         base58Prefixes[SCRIPT_ADDRESS] = 9;
