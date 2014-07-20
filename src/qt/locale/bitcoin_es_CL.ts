a9f7bc77bc977e3d0b05842a047f8dead39770d6
Coinworks.info <adamandshandra@server.local>
2014-07-20 13:18:47 -0600

    translation rebranding

--- src/qt/locale/bitcoin_es_CL.ts
+++ src/qt/locale/bitcoin_es_CL.ts
@@ -251,8 +251,8 @@ Eric Young (eay@cryptsoft.com) y UPnP software escrito por Thomas Bernard.</tran
     </message>
     <message>
         <location line="+1"/>
-        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR MYRIADCOINS&lt;/b&gt;!</source>
-        <translation>Atención: ¡Si codificas tu billetera y pierdes la contraseña perderás &lt;b&gt;TODOS TUS MYRIADCOINS&lt;/b&gt;!</translation>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR PARALLELCOINS&lt;/b&gt;!</source>
+        <translation>Atención: ¡Si codificas tu billetera y pierdes la contraseña perderás &lt;b&gt;TODOS TUS PARALLELCOINS&lt;/b&gt;!</translation>
     </message>
     <message>
         <location line="+0"/>
